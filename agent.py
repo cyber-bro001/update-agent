14a5aba74d92dff57cca1fa56e57032ace99ccc8
cyber-bro001 <okwuwavictor4@gmail.com>
2026-06-11 18:18:05 +0100

    added more commands to the agent

--- agent.py
+++ agent.py
@@ -21,6 +21,21 @@ while True:
   
       print("Tracking:", topic)
        
+    #untrack command
+  elif command.startswith("untrack "):
+     topic = command.replace("untrack ", "")
+
+     if topic in data["topics"]:
+        data["topics"].remove(topic)
+
+        with open("data.json", "w") as file:
+           json.dump(data, file, indent=2)
+
+        print("You are no longer tracking", topic)
+
+     else:
+        print(f"You are not tracking  {topic}  \nType 'list' to see a list of all the topics you're tracking.")
+
     #list command
   elif command == "list":
       if not data["topics"]:
@@ -29,13 +44,15 @@ while True:
       else:
        for topic in data["topics"]:
         print("-", topic)
-  
+
+  elif command == "help":
+     print(f"Available commands:\ntrack <topic> - add a topic to track\nuntrack <topic> - add a topic to untrack\nlist - list all tracked topics\nhelp - show this help menu\nexit - close command")
         
   else:
       print("unknown command")
        
   
-  if command == "exit":
-    break
+  if command == "exit": 
+   break
 
 
