10ebc4a74be2818d719c3b26d66aefd3123f1cc4
cyber-bro001 <okwuwavictor4@gmail.com>
2026-06-11 15:53:49 +0100

    updated agent witb a few commands

--- agent.py
+++ agent.py
@@ -3,33 +3,39 @@ import json
 with open("data.json", "r") as file:
     data = json.load(file)
 
-command = input("> ")
-
-#track command
-if command.startswith("track "):
+while True:
+  command = input("> ")
+  
+  #track command
+  if command.startswith("track "):
     topic = command.replace("track ", "")
-
+  
     if topic in data["topics"]:
-        print("You are already tracking this topic.")
-
+          print("You are already tracking this topic.")
+  
     else:
-    
-     data["topics"].append(topic)
+      data["topics"].append(topic)
+  
+      with open("data.json", "w") as file:
+          json.dump(data, file, indent=2)
+  
+      print("Tracking:", topic)
+       
+    #list command
+  elif command == "list":
+      if not data["topics"]:
+        print("You are not tracking any topic")
+  
+      else:
+       for topic in data["topics"]:
+        print("-", topic)
+  
+        
+  else:
+      print("unknown command")
+       
+  
+  if command == "exit":
+    break
 
-     with open("data.json", "w") as file:
-        json.dump(data, file, indent=2)
 
-     print("Tracking:", topic)
-
-#list command
-elif command.startswith("list "):
-    if not data["topics"]:
-      print("You are not tracking any topic")
-
-    else:
-     for topic in data["topics"]:
-      print("-", topic)
-
-      
-else:
-    print("unknown command")
